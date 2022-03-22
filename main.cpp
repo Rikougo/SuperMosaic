9331ad1dd60cda302e7899cf59f8957b0232a94a
Alexandre Spatola <alexandre.spatola@etu.umontpellier.fr>
2022-03-22 10:26:43 +0100

    Now configurable through cmd line

--- main.cpp
+++ main.cpp
@@ -19,52 +19,63 @@ auto lapTime(TP& tp)
     return d;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    ImageData img = loadImage("../images/renard_noel.png");
-    std::cout << "Image " << img.width << "x" << img.height << ", mean : " << img.mean << ", standard deviation : " << img.std_deviation << std::endl;
+    if(argc != 5)
+    {
+        std::cout << "Usage : " << argv[0] << " imgIn inBlockSize outBlockSize imgOut" << std::endl;
+        return -1;
+    }
+    ImageData img = loadImage(argv[1]);
+    int block_size = atoi(argv[2]);
+    int mosaic_block_size = atoi(argv[3]);
+    if(img.width % block_size || img.height % block_size)
+    {
+        std::cout << "Use an image whose size is a multiple of the block size!" << std::endl;
+        return -1;
+    }
 
     auto tp = std::chrono::high_resolution_clock::now();
     ImageDatabase db("../images/pets");
     std::cout << "Loading db took " << std::chrono::duration<float>(lapTime(tp)).count() << "s" << std::endl;
 
 
-    std::vector<std::size_t> indices(img.height / 16 * img.width / 16);
-    for (int j = 0; j < img.height / 16; ++j)
+    std::vector<std::size_t> indices(img.height / block_size * img.width / block_size);
+    for (int j = 0; j < img.height / block_size; ++j)
     {
-        for (int i = 0; i < img.width / 16; ++i)
+        for (int i = 0; i < img.width / block_size; ++i)
         {
             ImageBlockView block;
             block.img = &img;
-            block.height = block.width = 16;
-            block.x_start = i * 16;
-            block.y_start = j * 16;
-            indices[j * img.width / 16 + i] = db.findBestEntry(block);
+            block.height = block.width = block_size;
+            block.x_start = i * block_size;
+            block.y_start = j * block_size;
+            indices[j * img.width / block_size + i] = db.findBestEntry(block);
         }
     }
     std::cout << "Finding the best blocks took " << std::chrono::duration<float>(lapTime(tp)).count() << "s" << std::endl;
 
     OrderedDirectory dir("../images/pets");
     ImageData mosaic;
-    mosaic.pixels.resize(img.height * 8ull * img.width * 8);
-    mosaic.height = img.height * 8;
-    mosaic.width = img.width * 8;
-    for (int j = 0; j < img.height / 16; ++j)
+    mosaic.height = img.height / block_size * mosaic_block_size;
+    mosaic.width = img.width / block_size * mosaic_block_size;
+    mosaic.pixels.resize((std::size_t)mosaic.height * mosaic.width);
+    for (int j = 0; j < img.height / block_size; ++j)
     {
-        for (int i = 0; i < img.width / 16; ++i)
+        for (int i = 0; i < img.width / block_size; ++i)
         {
             ImageBlockView block;
             block.img = &mosaic;
-            block.height = block.width = 128;
-            block.x_start = i * 128;
-            block.y_start = j * 128;
-            ImageData tile = loadImage(dir[indices[j * img.width / 16 + i]].string().c_str());
+            block.height = block.width = mosaic_block_size;
+            block.x_start = i * mosaic_block_size;
+            block.y_start = j * mosaic_block_size;
+            ImageData tile = loadImage(dir[indices[j * img.width / block_size + i]].string().c_str());
             stbir_resize_float(&tile.pixels[0].r, tile.width, tile.height, 0, &block(0,0).r, block.width, block.height, block.img->width * sizeof(RgbPixel), 3);
         }
     }
     std::cout << "Building the mosaic took " << std::chrono::duration<float>(lapTime(tp)).count() << "s" << std::endl;
 
-    saveImage(mosaic, "data.png");
+    saveImage(mosaic, argv[4]);
 
     return EXIT_SUCCESS;
 }
